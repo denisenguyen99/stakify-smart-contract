0544b217b203ef97937e1070bc260b28c26bfc98
Tai Nguyen <nguyenductai24081996@gmail.com>
2023-06-10 13:50:20 +0700

    update execute add reward

--- contracts/campaign/src/contract.rs
+++ contracts/campaign/src/contract.rs
@@ -1,13 +1,11 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
-use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, to_binary, Uint128};
+use cosmwasm_std::{Binary, CosmosMsg, Deps, DepsMut, Env, MessageInfo, Response, SubMsg, to_binary, Uint128, WasmMsg};
 use cw2::set_contract_version;
-// use cw2::set_contract_version;
-
+use cw20::Cw20ExecuteMsg;
 use crate::error::ContractError;
 use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 use crate::state::{CAMPAIGN_INFO, CampaignInfo};
-
 // version info for migration info
 const CONTRACT_NAME: &str = "crates.io:campaign";
 const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
@@ -22,17 +20,23 @@ pub fn instantiate(
     set_contract_version(_deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
 
     let campaign = &CampaignInfo {
+        owner: _deps.api.addr_validate(&_msg.owner)?.to_string(),
         collection: _deps.api.addr_validate(&_msg.collection)?.to_string(),
-        reward_token: _msg.reward_token.clone(),
         reward_per_second: Uint128::zero(),
         start_time: _msg.start_time,
         end_time: _msg.end_time,
+        reward_token_amount: _msg.reward_token_amount,
+        reward_token_available: Uint128::zero(),
+        reward_token_address: _msg.reward_token_address.to_string(),
     };
 
     CAMPAIGN_INFO.save(_deps.storage, campaign)?;
 
     Ok(Response::new().add_attributes([
         ("action", "instantiate"),
+        ("collection", &_msg.collection.to_string()),
+        ("reward_token_address", &_msg.reward_token_address.to_string()),
+        ("reward_token_amount", &_msg.reward_token_amount.to_string()),
         ("start_time", &_msg.start_time.to_string()),
         ("end_time", &_msg.end_time.to_string()),
     ]))
@@ -40,12 +44,45 @@ pub fn instantiate(
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn execute(
-    _deps: DepsMut,
-    _env: Env,
-    _info: MessageInfo,
-    _msg: ExecuteMsg,
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo,
+    msg: ExecuteMsg,
 ) -> Result<Response, ContractError> {
-    unimplemented!()
+    match msg {
+        ExecuteMsg::AddRewardToken {} => execute_add_reward_balance(deps, env, info),
+    }
+}
+
+pub fn execute_add_reward_balance(
+    deps: DepsMut,
+    env: Env,
+    info: MessageInfo
+) -> Result<Response, ContractError> {
+    let mut campaign_info: CampaignInfo = CAMPAIGN_INFO.load(deps.storage)?;
+
+    if campaign_info.owner != info.sender {
+        return Err(ContractError::Unauthorized {});
+    }
+
+    let mut res = Response::new();
+
+    let transfer = SubMsg::new(CosmosMsg::Wasm(WasmMsg::Execute {
+        contract_addr: campaign_info.reward_token_address,
+        msg: to_binary(&Cw20ExecuteMsg::Transfer {
+            recipient: env.contract.address.clone().to_string(),
+            amount: campaign_info.reward_token_amount,
+        })?,
+        funds: vec![],
+    }));
+
+    res = res.add_submessages(transfer);
+
+    campaign_info.reward_token_available = campaign_info.reward_token_amount;
+
+    CAMPAIGN_INFO.save(deps.storage, &campaign_info)?;
+
+    Ok(res)
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
@@ -61,4 +98,43 @@ fn query_campaign_info(deps: Deps) -> Result<CampaignInfo, ContractError> {
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use super::*;
+    use cosmwasm_std::testing::{
+        mock_dependencies, mock_env, MockApi, MockQuerier, MockStorage, mock_info
+    };
+
+    #[test]
+    fn test_instantiate() {
+        // Tạo các đối tượng giả lập cho môi trường thử nghiệm
+        let api = MockApi::default();
+        let storage = MockStorage::new();
+        let querier = MockQuerier::new(&[]);
+        let mut deps = mock_dependencies();
+
+        // Tạo một môi trường thử nghiệm giả lập
+        let env = mock_env();
+
+        // Gọi hàm trong smart contract và lấy kết quả
+
+        let result = instantiate(
+            deps.as_mut(),
+            env, mock_info("tai", &[]),
+            InstantiateMsg{
+                collection: "".to_string(),
+                reward_token_address: "".to_string(),
+                reward_token_amount: Default::default(),
+                reward_token_available: Default::default(),
+                start_time: 0,
+                end_time: 0,
+                owner: "".to_string(),
+            }
+        ).unwrap();
+
+        // Kiểm tra kết quả hợp lệ
+        assert_eq!(result, true);
+        // Nếu cần, kiểm tra giá trị trả về
+        // assert_eq!(result.unwrap().value, expected_value);
+    }
+}
+
--- contracts/campaign/src/msg.rs
+++ contracts/campaign/src/msg.rs
@@ -1,26 +1,43 @@
 use cosmwasm_schema::{cw_serde, QueryResponses};
-use crate::state::RewardTokenInfo;
+use cosmwasm_std::Uint128;
 
 #[cw_serde]
 pub struct InstantiateMsg {
-    /// Reward Token address (CW20 or Native)
+    /// collection
     pub collection: String,
-    /// Reward Token address (CW20 or Native)
-    pub reward_token: RewardTokenInfo,
+    /// Token denom
+    pub reward_token_address: String,
+    /// Token denom
+    pub reward_token_amount: Uint128,
+    /// Start time
+    pub reward_token_available: Uint128,
     /// Start time
     pub start_time: u64,
     /// End time
     pub end_time: u64,
+    /// owner
+    pub owner: String,
 }
 
 #[cw_serde]
 pub enum ExecuteMsg {
-
+    AddRewardToken {},
+    // DepositNft {
+    //     owner: String,
+    //     nft: String,
+    //     start: Uint128
+    // },
+    // ClaimReward {
+    //     owner: String
+    // },
+    // WithdrawNft {
+    //     owner: String,
+    //     nft: String
+    // }
 }
 
 #[cw_serde]
-#[derive(QueryResponses)]
 pub enum QueryMsg {
-    #[returns(CampaignInfo)]
+    // #[returns(CampaignInfo)]
     Campaign {}
 }
